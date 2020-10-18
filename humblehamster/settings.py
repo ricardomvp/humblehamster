3997c6cca7d540dc161efc2ebe756cbf51dac904
RicardoMV <ricardom.ipn@gmail.com>
2020-10-18 16:01:00 -0500

    changes in settings Whitenoise

--- humblehamster/settings.py
+++ humblehamster/settings.py
@@ -125,3 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
