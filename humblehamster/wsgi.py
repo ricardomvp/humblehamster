ee1a296714c0da66bc6d6d802e1c2803657a8051
RicardoMV <ricardom.ipn@gmail.com>
2020-10-29 14:21:47 -0600

    .

--- humblehamster/wsgi.py
+++ humblehamster/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'humblehamster.settings')
 
 application = get_wsgi_application()
